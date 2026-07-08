c2164d08e5c145fc6de793cc1e9697c1657be6c7
Developer <user@example.com>
2026-07-08 11:29:34 +0530

    ignored opplementary from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: [
+    "@opentelemetry/instrumentation-winston",
+    "@opentelemetry/winston-transport",
+  ],
 };
 
 export default nextConfig;
