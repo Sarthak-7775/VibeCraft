8f7be7f0176378c84b79a2bb9fe2de2118be81ae
Developer <user@example.com>
2026-07-31 20:18:38 +0530

    e2b sandbox timeout incresed

--- src/inngest/types.ts
+++ src/inngest/types.ts
@@ -1,1 +1,2 @@
-export const SANDBOX_TIMEOUT = 60_000 * 10 * 3; // 30 minutes in MS
+export const SANDBOX_TIMEOUT = 24 * 60 * 60 * 1000;
+// 1 day in milli secconds --- change it later if e2b charges more
